6c246bc69ebe94f55ebec4f5e3faa681847b36d6
Diego Rodriguez <101908524+Haonter@users.noreply.github.com>
2022-11-20 04:59:06 -0300

    Upload Files

---
+++ index.js
@@ -0,0 +1,133 @@
+let IMG = document.getElementById("VideoIMG");
+let IMG2 = document.getElementById("VideoIMG2");
+let IMG3 = document.getElementById("VideoIMG3");
+let IMG4 = document.getElementById("VideoIMG4");
+let IMG5 = document.getElementById("VideoIMG5");
+let IMG6 = document.getElementById("VideoIMG6");
+let IMG7 = document.getElementById("VideoIMG7");
+let IMG8 = document.getElementById("VideoIMG8");
+let IMG9 = document.getElementById("VideoIMG9");
+let IMGA = document.getElementById("VideoIMGA");
+let IMGB = document.getElementById("VideoIMGB");
+let IMGC = document.getElementById("VideoIMGC");
+
+
+function imgEntrar(){
+    setTimeout(cambio,10);
+    function cambio(){
+        IMG.src='Imagenes_Youtube/doom.webp';
+    }
+}
+function imgSalir(){
+    IMG.src='Imagenes_Youtube/doom.jpg';
+}
+
+function imgEntrar2(){
+    setTimeout(cambio,10);
+    function cambio(){
+        IMG2.src='Imagenes_Youtube/nate2.webp';
+    }
+}
+function imgSalir2(){
+    IMG2.src='Imagenes_Youtube/nate.webp';
+}
+
+function imgEntrar3(){
+    setTimeout(cambio,10);
+    function cambio(){
+        IMG3.src='Imagenes_Youtube/wss2.webp';
+    }
+}
+function imgSalir3(){
+    IMG3.src='Imagenes_Youtube/wss.webp';
+}
+
+function imgEntrar4(){
+    setTimeout(cambio,10);
+    function cambio(){
+        IMG4.src='Imagenes_Youtube/gta2.webp';
+    }
+}
+function imgSalir4(){
+    IMG4.src='Imagenes_Youtube/gta.webp';
+}
+
+function imgEntrar5(){
+    setTimeout(cambio,10);
+    function cambio(){
+        IMG5.src='Imagenes_Youtube/wss4.webp';
+    }
+}
+function imgSalir5(){
+    IMG5.src='Imagenes_Youtube/wss3.webp';
+}
+
+function imgEntrar6(){
+    setTimeout(cambio,10);
+    function cambio(){
+        IMG6.src='Imagenes_Youtube/luisito2.webp';
+    }
+}
+function imgSalir6(){
+    IMG6.src='Imagenes_Youtube/luisito.webp';
+}
+
+function imgEntrar7(){
+    setTimeout(cambio,10);
+    function cambio(){
+        IMG7.src='Imagenes_Youtube/bluuweb2.webp';
+    }
+}
+function imgSalir7(){
+    IMG7.src='Imagenes_Youtube/bluuweb.webp';
+}
+
+function imgEntrar8(){
+    setTimeout(cambio,10);
+    function cambio(){
+        IMG8.src='Imagenes_Youtube/ME2.webp';
+    }
+}
+function imgSalir8(){
+    IMG8.src='Imagenes_Youtube/ME.webp';
+}
+
+function imgEntrar9(){
+    setTimeout(cambio,10);
+    function cambio(){
+        IMG9.src='Imagenes_Youtube/i92.webp';
+    }
+}
+function imgSalir9(){
+    IMG9.src='Imagenes_Youtube/i9.webp';
+}
+
+function imgEntrarA(){
+    setTimeout(cambio,10);
+    function cambio(){
+        IMGA.src='Imagenes_Youtube/Ann2.webp';
+    }
+}
+function imgSalirA(){
+    IMGA.src='Imagenes_Youtube/Ann.webp';
+}
+
+function imgEntrarB(){
+    setTimeout(cambio,10);
+    function cambio(){
+        IMGB.src='Imagenes_Youtube/Skyharbor2.webp';
+    }
+}
+function imgSalirB(){
+    IMGB.src='Imagenes_Youtube/Skyharbor.webp';
+}
+
+function imgEntrarC(){
+    setTimeout(cambio,10);
+    function cambio(){
+        IMGC.src='Imagenes_Youtube/Tesseract2.webp';
+    }
+}
+function imgSalirC(){
+    IMGC.src='Imagenes_Youtube/Tesseract.webp';
+}
